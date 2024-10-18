ecc5a57d70c6ec63d85387fcc7a316a356733eba
venkatesh-tc <venkatesh@targetclose.com>
2024-10-18 19:20:01 +0530

    Add files via upload

---
+++ php-practice/Helloworld.php
@@ -0,0 +1,11 @@
+Hello everyone!
+
+
+
+
+C:\Users\tcind\AppData\Roaming\Python\Python312\Scripts'
+
+
+{{ url_for('user_bp.static', filename='css/style.css') }}
+
+{{ url_for('user_bp.static', filename='css/styles.css') }}
